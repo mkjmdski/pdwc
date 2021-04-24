08662c4f242b81d2ac2a30de7b7b66103bc3f8fc
Jakub Kasprzak <jakub.kasprzak@sqlpedia.pl>
2021-04-24 16:05:48 +0200

    Materiały na zajęcia

--- labs/terraform/solutions/main.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  common_tags = {
-    Purpose = "UAM Cloud Data Processing"
-    Environment = "DEV"
-    Owner = var.student_full_name
-  }
-}
--- labs/terraform/solutions/s3.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_s3_bucket" "main_dl_bucket" {
-  bucket = "datalake-${var.environment}-${var.account_number}-${var.student_initials}-${var.student_index_no}"
-  force_destroy = true
-
-  tags = merge(local.common_tags, )
-}
