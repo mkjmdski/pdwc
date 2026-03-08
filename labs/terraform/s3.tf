3a2f4ea42fde0ffc4b74507d5487a27f9209eef2
Mikołaj Młodzikowski <mikolaj.mlodzikowski@olx.pl>
2026-03-08 10:21:22 +0100

    feat: change names

--- labs/terraform/s3.tf
+++ labs/terraform/s3.tf
@@ -1,8 +1,7 @@
 resource "aws_s3_bucket" "landings" {
   for_each = toset([
-    "gold",
-    "silver",
-    "bronze",
+    "raw",
+    "processed",
   ])
   bucket        = "datalake-${each.key}-${local.identifier}"
   force_destroy = true
