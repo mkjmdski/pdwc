a9242a74011e28780e900b6303fe5ad054f944a4
Mikołaj Młodzikowski <mikolaj.mlodzikowski@olx.pl>
2026-03-21 10:08:46 +0100

    feat: add query

---
+++ labs/sql_queries.sql
@@ -0,0 +1,38 @@
+WITH hourly_trades AS (
+    SELECT
+        date_format(from_unixtime(transaction_ts), '%Y-%m-%dT%H') AS HourlyBucket,
+        ROW_NUMBER() OVER (
+            PARTITION BY symbol, "type",
+                         date_format(from_unixtime(transaction_ts), '%Y-%m-%dT%H')
+            ORDER BY dollar_amount DESC
+        ) AS rnk,
+        transaction_ts,
+        symbol,
+        price,
+        amount,
+        dollar_amount,
+        "type",
+        trans_id,
+        date_format(from_unixtime(transaction_ts), '%Y')  AS "year",
+        month,
+        day,
+        hour
+    FROM datalake_raw_174202832180_mm_s1203137.crawler_year_2026
+)
+SELECT
+    HourlyBucket,
+    rnk,
+    transaction_ts,
+    symbol,
+    price,
+    amount,
+    dollar_amount,
+    "type",
+    trans_id,
+    "year",
+    month,
+    day,
+    hour
+FROM hourly_trades
+WHERE rnk = 1
+ORDER BY HourlyBucket, symbol, "type";
