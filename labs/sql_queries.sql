733eaa68d5a652652b80e8820526fe63f7a75ceb
Mikołaj Młodzikowski <mikolaj.mlodzikowski@olx.pl>
2026-03-21 10:14:33 +0100

    feat: reformat query

--- labs/sql_queries.sql
+++ labs/sql_queries.sql
@@ -2,8 +2,10 @@ WITH hourly_trades AS (
     SELECT
         date_format(from_unixtime(transaction_ts), '%Y-%m-%dT%H') AS HourlyBucket,
         ROW_NUMBER() OVER (
-            PARTITION BY symbol, "type",
-                         date_format(from_unixtime(transaction_ts), '%Y-%m-%dT%H')
+            PARTITION BY
+                symbol,
+                "type",
+                date_format(from_unixtime(transaction_ts), '%Y-%m-%dT%H')
             ORDER BY dollar_amount DESC
         ) AS rnk,
         transaction_ts,
@@ -19,20 +21,7 @@ WITH hourly_trades AS (
         hour
     FROM datalake_raw_174202832180_mm_s1203137.crawler_year_2026
 )
-SELECT
-    HourlyBucket,
-    rnk,
-    transaction_ts,
-    symbol,
-    price,
-    amount,
-    dollar_amount,
-    "type",
-    trans_id,
-    "year",
-    month,
-    day,
-    hour
+SELECT *
 FROM hourly_trades
 WHERE rnk = 1
 ORDER BY HourlyBucket, symbol, "type";
